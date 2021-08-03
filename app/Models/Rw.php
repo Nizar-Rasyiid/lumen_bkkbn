b81825e0d43d5af4f9d4dbd9efddf215e69fe48b
dzul_kifl1 <60170808+dzulkifli-mq@users.noreply.github.com>
2021-08-03 12:36:36 +0700

    RtRw

---
+++ app/Models/Rw.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Rw extends Model
+{
+    protected $table    = 'rw';
+    //public $incrementing = false;
+    protected $primaryKey = 'id_rw';
+    public $timestamps = false;
+    protected $fillable = ['id_rw', 'nama_rw', 'KodeDepdagri','IsActive','id_kelurahan'];
+    
+    public function KelurahanId()
+    {
+        return $this->belongsTo('App\Models\Kelurahan', 'id_kelurahan', 'id_kelurahan');
+    }
+    public function KabupatenKotaID()
+    {
+        return $this->belongsTo('App\Models\Kabupaten', 'id_kabupaten', 'id_kabupaten');
+    }
+
+    public function ProvinsiKotaID()
+    {
+        return $this->belongsTo('App\Models\Provinsi', 'id_provinsi', 'id_provinsi');
+    }
+
+    public function KecamatanId()
+    {
+        return $this->belongsTo('App\Models\Kecamatan', 'id_kecamatan', 'id_kecamatan');
+    }
+
+}
