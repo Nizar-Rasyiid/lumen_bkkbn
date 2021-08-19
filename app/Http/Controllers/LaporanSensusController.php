441bfc5d2179e7b1633dfcb8bb4b634d35804677
Nizar-Rasyiid <nizarrasyiid1221@gmail.com>
2021-08-19 22:28:32 +0700

    show yang bener

--- app/Http/Controllers/LaporanSensusController.php
+++ app/Http/Controllers/LaporanSensusController.php
@@ -35,19 +35,21 @@ class LaporanSensusController extends Controller
         target_sensus_indo.jumKel
         FROM (SELECT 
         id_provinsi,
-        id_kecamatan,
-        id_kelurahan,
+        -- id_kecamatan,
+        -- id_kelurahan,
         Periode_Sensus,   
         sum(target_kk) as KK,
         count(DISTINCT(id_kabupaten)) as jumKab, 
         count(DISTINCT(id_kecamatan)) as jumKec, 
         count(DISTINCT(id_kelurahan)) as jumKel 
-        FROM Target_KK GROUP BY id_provinsi,id_kecamatan,id_kelurahan,Periode_Sensus) target_sensus_indo
+        FROM Target_KK GROUP BY id_provinsi,Periode_Sensus
+        HAVING Periode_Sensus = $Periode_Sensus
+        ) target_sensus_indo
         INNER JOIN provinsi ON target_sensus_indo.id_provinsi = provinsi.id_provinsi
-        INNER JOIN kecamatan ON  target_sensus_indo.id_kecamatan = kecamatan.id_kecamatan
-        INNER JOIN kelurahan ON  target_sensus_indo.id_kelurahan = kelurahan.id_kelurahan
-        INNER JOIN target_kk ON target_sensus_indo.Periode_Sensus = target_kk.Periode_Sensus
-        WHERE target_kk.Periode_Sensus = $Periode_Sensus
+        -- INNER JOIN kecamatan ON  target_sensus_indo.id_kecamatan = kecamatan.id_kecamatan
+        -- INNER JOIN kelurahan ON  target_sensus_indo.id_kelurahan = kelurahan.id_kelurahan
+        -- INNER JOIN target_kk ON target_sensus_indo.Periode_Sensus = target_kk.Periode_Sensus
+        -- WHERE target_kk.Periode_Sensus = $Periode_Sensus
         "
         ));
 
