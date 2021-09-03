acaca0926de92ff0aa9cec6b66dc821eeb785edf
dzul_kifl1 <santriquarta@gmail.com>
2021-09-03 14:38:58 +0700

    p

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -29,21 +29,18 @@
                 font-weight: 400;
                 src: local('Lato Regular'), local('Lato-Regular'), url(https://fonts.gstatic.com/s/lato/v11/qIIYRU-oROkIk8vfvxw6QvesZW2xOQ-xsNqO47m55DA.woff) format('woff');
             }
-
             @font-face {
                 font-family: 'Lato';
                 font-style: normal;
                 font-weight: 700;
                 src: local('Lato Bold'), local('Lato-Bold'), url(https://fonts.gstatic.com/s/lato/v11/qdgUG4U09HnJwhYI-uK18wLUuEpTyoUstqEm5AMlJo4.woff) format('woff');
             }
-
             @font-face {
                 font-family: 'Lato';
                 font-style: italic;
                 font-weight: 400;
                 src: local('Lato Italic'), local('Lato-Italic'), url(https://fonts.gstatic.com/s/lato/v11/RYyZNoeFgb0l7W3Vu1aSWOvvDin1pK8aKteLpeZ5c0A.woff) format('woff');
             }
-
             @font-face {
                 font-family: 'Lato';
                 font-style: italic;
@@ -51,7 +48,6 @@
                 src: local('Lato Bold Italic'), local('Lato-BoldItalic'), url(https://fonts.gstatic.com/s/lato/v11/HkF_qI1x_noxlxhrhMQYELO3LdcAZYWl9Si6vvxL-qU.woff) format('woff');
             }
         }
-
         /* CLIENT-SPECIFIC STYLES */
         body,
         table,
@@ -60,17 +56,14 @@
             -webkit-text-size-adjust: 100%;
             -ms-text-size-adjust: 100%;
         }
-
         table,
         td {
             mso-table-lspace: 0pt;
             mso-table-rspace: 0pt;
         }
-
         img {
             -ms-interpolation-mode: bicubic;
         }
-
         /* RESET STYLES */
         img {
             border: 0;
@@ -79,18 +72,15 @@
             outline: none;
             text-decoration: none;
         }
-
         table {
             border-collapse: collapse !important;
         }
-
         body {
             height: 100% !important;
             margin: 0 !important;
             padding: 0 !important;
             width: 100% !important;
         }
-
         /* iOS BLUE LINKS */
         a[x-apple-data-detectors] {
             color: inherit !important;
@@ -100,7 +90,6 @@
             font-weight: inherit !important;
             line-height: inherit !important;
         }
-
         /* MOBILE STYLES */
         @media screen and (max-width:600px) {
             h1 {
@@ -108,7 +97,6 @@
                 line-height: 32px !important;
             }
         }
-
         /* ANDROID CENTER FIX */
         div[style*="margin: 16px 0;"] {
             margin: 0 !important;
